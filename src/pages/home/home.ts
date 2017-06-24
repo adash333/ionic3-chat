e2c55fba4a3eb0c8edc3e27d0eeabea3b6346f24
adash333 <adash333@hotmail.com>
2017-06-24 12:41:56 +0900

    ngIf and click event => chat comment

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Comment } from '../../models/chat'; // 追加
+import { Comment, User } from '../../models/chat'; // 追加
 
 @Component({
   selector: 'page-home',
@@ -8,7 +8,28 @@ import { Comment } from '../../models/chat'; // 追加
 })
 export class HomePage {
 
+  CURRENT_USER = new User(1, 'Tanaka Jiro'); // 自分のUser情報を追加
+  ANOTHER_USER = new User(2, 'Suzuki Taro'); // 相手のUser情報を追加
+
+  COMMENTS: Comment[] = [ // クラスを元にコメントを作成
+    new Comment(this.ANOTHER_USER, 'Suzukiの１つ目のコメントです。'),
+    new Comment(this.ANOTHER_USER, 'Suzukiの2つ目のコメントです。'),
+    new Comment(this.CURRENT_USER, 'Tanakaの１つ目のコメントです。'),
+    new Comment(this.ANOTHER_USER, 'Suzukiの3つ目のコメントです。'),
+    new Comment(this.CURRENT_USER, 'Tanakaの2つ目のコメントです。')
+  ];
+
   public content = '';
+  public comments = this.COMMENTS;
+  public current_user = this.CURRENT_USER;
+
+  // 新しいコメントを追加
+  addComment(comment: string) {
+     if (comment) {
+       this.comments.push(new Comment(this.current_user, comment));
+     }
+  }
+
   /*
   const COMMENTS: Comment[] = [
     { name: "Suzuki Taro",  content: "１つ目のコメントです。"},
@@ -16,12 +37,13 @@ export class HomePage {
     { name: "Suzuki Taro",  content: "３つ目のコメントです。"}
   ]
   */
-
+  /*
   comments:[{ name: string, content: string }] = [
     { name: "Suzuki Taro",  content: "１つ目のコメントです。"},
     { name: "Suzuki Taro",  content: "２つ目のコメントです。"},
     { name: "Suzuki Taro",  content: "３つ目のコメントです。"}
   ]
+  */
 
   constructor(public navCtrl: NavController) {
 
