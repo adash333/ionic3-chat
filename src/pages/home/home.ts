53003bc6070fbd105916ea24265a9c133aa8c455
adash333 <adash333@hotmail.com>
2017-06-24 10:06:27 +0900

    ngFor de chat comment

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Comment } from '../../models/chat'; // 追加
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  public content = '';
+  /*
+  const COMMENTS: Comment[] = [
+    { name: "Suzuki Taro",  content: "１つ目のコメントです。"},
+    { name: "Suzuki Taro",  content: "２つ目のコメントです。"},
+    { name: "Suzuki Taro",  content: "３つ目のコメントです。"}
+  ]
+  */
+
+  comments:[{ name: string, content: string }] = [
+    { name: "Suzuki Taro",  content: "１つ目のコメントです。"},
+    { name: "Suzuki Taro",  content: "２つ目のコメントです。"},
+    { name: "Suzuki Taro",  content: "３つ目のコメントです。"}
+  ]
 
   constructor(public navCtrl: NavController) {
 
