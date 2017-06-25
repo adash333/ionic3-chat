77969c4b6187809bb6d91bcfdf27f4366eaadb5a
adash333 <adash333@hotmail.com>
2017-06-25 23:08:21 +0900

    CRUD

--- src/models/chat.ts
+++ src/models/chat.ts
@@ -1,6 +1,6 @@
-import * as moment from 'moment'; // 追加
+import * as moment from 'moment';
 
-export class User { //Userの定義を追加
+export class User {
   uid: number;
   name: string;
 
@@ -10,16 +10,26 @@ export class User { //Userの定義を追加
   }
 }
 
-export class Comment { //Commentの定義を変更
+export class Comment {
   user: User;
   initial: string;
   content: string;
-  date: number;         // 追加
+  date: number;
+  key?: string; // 追加
+  edit_flag?: boolean; // 追加
 
   constructor(user: User, content: string) {
     this.user = user;
     this.initial = user.name.slice(0, 1);
     this.content = content;
-    this.date = +moment(); // 追加
+    this.date = +moment();
+  }
+
+  // 取得した日付を反映し、更新フラグをつける
+  setData(value: any): Comment {
+    this.date = value.date;
+    this.key = value.$key // 追加
+    this.edit_flag = false; // 追加
+    return this;
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Comment, User } from '../../models/chat'; // 追加
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database'; // 追加
+import { AlertController } from 'ionic-angular';  // 追加
 
 @Component({
   selector: 'page-home',
@@ -12,45 +13,53 @@ export class HomePage {
   CURRENT_USER = new User(1, 'Tanaka Jiro'); // 自分のUser情報を追加
   ANOTHER_USER = new User(2, 'Suzuki Taro'); // 相手のUser情報を追加
 
-  COMMENTS: Comment[] = [ // クラスを元にコメントを作成
-    new Comment(this.ANOTHER_USER, 'Suzukiの１つ目のコメントです。'),
-    new Comment(this.ANOTHER_USER, 'Suzukiの2つ目のコメントです。'),
-    new Comment(this.CURRENT_USER, 'Tanakaの１つ目のコメントです。'),
-    new Comment(this.ANOTHER_USER, 'Suzukiの3つ目のコメントです。'),
-    new Comment(this.CURRENT_USER, 'Tanakaの2つ目のコメントです。')
-  ];
-
-  items: FirebaseListObservable<any[]>; // 追加
-
+  public FB_comments: FirebaseListObservable<any[]>; // 追加
   public content = '';
-  public comments = this.COMMENTS;
+  public comments: Comment[] = [];  // 更新
   public current_user = this.CURRENT_USER;
 
   // DI（依存性注入する機能を指定）
-  constructor(public navCtrl: NavController, public db: AngularFireDatabase, ) {
-    this.items = db.list('/item');
+  constructor(public navCtrl: NavController, public db: AngularFireDatabase, public alertCtrl: AlertController) {
+    this.FB_comments = db.list('/comments'); // 追加
+    this.FB_comments.subscribe((snapshots: any[]) => {
+      this.comments = []; // コメントを初期化
+      snapshots.forEach((snapshot: any) => {
+        // 取得したデータを反映
+        this.comments.push(new Comment(snapshot.user, snapshot.content).setData(snapshot));
+      });
+    });
   }
 
   // 新しいコメントを追加
   addComment(comment: string) {
      if (comment) {
-       this.comments.push(new Comment(this.current_user, comment));
+       this.FB_comments.push(new Comment(this.current_user, comment)); // 更新
+       this.content = '';
      }
   }
 
-  /*
-  const COMMENTS: Comment[] = [
-    { name: "Suzuki Taro",  content: "１つ目のコメントです。"},
-    { name: "Suzuki Taro",  content: "２つ目のコメントです。"},
-    { name: "Suzuki Taro",  content: "３つ目のコメントです。"}
-  ]
-  */
-  /*
-  comments:[{ name: string, content: string }] = [
-    { name: "Suzuki Taro",  content: "１つ目のコメントです。"},
-    { name: "Suzuki Taro",  content: "２つ目のコメントです。"},
-    { name: "Suzuki Taro",  content: "３つ目のコメントです。"}
-  ]
-  */
+  // 編集フィールドの切り替え
+  toggleEditComment(num: number) {
+    this.comments[num].edit_flag = (this.comments[num].edit_flag) ? false : true;
+  }
+
+  // コメントを更新する
+  saveEditComment(num: number, key: string) {
+    this.FB_comments.update(key, {
+      content: this.comments[num].content, 
+      date: this.comments[num].date
+    });
+    this.comments[num].edit_flag = false;
+  }
+
+  // コメントをリセットする
+  resetEditComment(num: number) {
+    this.comments[num].content = '';
+  }
+
+  // コメントを削除する
+  deleteComment(key: string) {
+    this.FB_comments.remove(key);
+  }
 
 }
